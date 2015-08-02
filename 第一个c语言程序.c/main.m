37f662b9e7d4f67f3b780f2c4e0e12dce3b86d09
ios－25 <ios-25@ACC80270.ipt.aol.com>
2015-08-01 22:48:46 -0700

    Initial Commit

---
+++ 第一个c语言程序.c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  第一个c语言程序.c
+//
+//  Created by ios－25 on 15/8/1.
+//  Copyright (c) 2015年 ios－25. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
